c4575e9e345e91bb47b3fc15d1a5d0b48e89ebcd
Odelia Finkelstein <odeliafinkelstein@Odelias-MacBook-Air.local>
2021-03-16 13:58:56 -0400

    Created InterviewerListItem Component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import 'components/InterviewerListItem.scss';
+import classnames from 'classnames';
 
 export default function InterviewListItem(props) {
+  const interviewListItemClass = classnames('interviews__item', {
+    'interviews__item--selected': props.selected,
+  });
+
+  const interviewerImageClass = classnames('interviewers__item-image', {
+    'interviewers__item-image--selected': props.selected,
+  });
+
   return (
-    <li className="interviews__item">
+    <li
+      className={interviewListItemClass}
+      onClick={() => props.setInterviewer(props.name)}
+    >
       <img
-        className="interviewers__item-image"
+        className={interviewerImageClass}
         src="https://i.imgur.com/LpaY82x.png"
         alt="Sylvia Palmer"
       />
+      {props.selected && props.name}
     </li>
   );
 }
