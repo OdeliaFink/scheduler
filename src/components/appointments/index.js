0b11db4ab793b0166de080501727285d81780d99
Odelia Finkelstein <odeliafinkelstein@Odelias-MacBook-Air.local>
2021-03-16 20:42:40 -0400

    Updated Appointment Component by Imported Header, Show and Empty to Display Appointments

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 import 'components/appointments/styles.scss';
 
 export default function Appointment(props) {
-  return <article className="Appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
