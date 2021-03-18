492ba826b37623527e3ee602d4a2028c0f45373c
Odelia Finkelstein <odeliafinkelstein@Odelias-MacBook-Air.local>
2021-03-18 17:15:13 -0400

    Added transition property in useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,11 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
+  function useCustomHook() {
+    function action() {}
+
+    return { action };
+  }
+
   return { mode };
 }
